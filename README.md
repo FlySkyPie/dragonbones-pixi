d8e61e1de5a8bb2a3eacda7f830f89a10203e7e9
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2022-12-31 08:33:23 +0800

    build: Update config

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This library is fork/extract from [DragonBonesJS](https://github.com/DragonBones/DragonBonesJS), and refactoring to modern Javascript package (ES Module).
 
 This library supported 4.X of PixiJS.
+
+## Install
+
+```
+npm install @flyskypie/dragonbones-pixi pixi.js@^4.0.0
+```
+
+or 
+
+```
+yarn add @flyskypie/dragonbones-pixi pixi.js@^4.0.0
+```
--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,69 @@ import path from "path";
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@dragonbones-pixi": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      resolve: {
+        alias: {
+          "@dragonbones-pixi": path.resolve(__dirname, "./src"),
+        },
+      },
+      define: {
+        global: '({})'
+      },
+    }
+  };
+
+  if (mode === 'demo') {
+    const input = [
+      "AnimationBase",
+      "BoundingBox",
+      "EyeTracking",
+      "InverseKinematics",
+      "PerformanceTest",
+      "ReplaceSkin",
+      "AnimationLayer",
+      "BoneOffset",
+      "CoreElement",
+      "DragonBonesEvent",
+      "HelloDragonBones",
+      "MultiTextureAltas",
+      "ReplaceAnimation",
+      "ReplaceSlotDisplay",
+    ].reduce<{ [key: string]: string }>((t, name) => {
+      t[name] = path.resolve(__dirname, `demo/${name}/index.html`);
+      return t;
+    }, {})
+
+    return {
+      resolve: {
+        alias: {
+          "@dragonbones-pixi": path.resolve(__dirname, "./src"),
+        },
+      },
+      build: {
+        outDir: path.resolve(__dirname, 'dist-demo'),
+        rollupOptions: {
+          input,
+        }
+      },
+    }
+  }
+
+  return {
+    publicDir: false,
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'dragonBonesPixi',
+        //formats: ['es'],
+        fileName: 'dragonbones-pixi',
+      },
+      rollupOptions: {
+        external: "pixi.js",
+      }
     },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'dragonBonesPixi',
-      formats: ['es'],
-      fileName: 'dragonbones-pixi',
-    },
-  },
-  plugins: [dts({ rollupTypes: true })],
-  define: {
-    global: {},
-  },
+    plugins: [dts({ rollupTypes: true })],
+  }
 });
